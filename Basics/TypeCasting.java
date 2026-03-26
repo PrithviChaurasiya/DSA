1b1184b8cebd8638d5f438e6add577c1ab71e968
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-03-26 20:18:37 +0530

    Add Modulus Operator file

---
+++ Basics/TypeCasting.java
@@ -0,0 +1,17 @@
+package DSA.Basics;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        //Character To Integer
+        char ch = 'A';
+        System.out.println(ch); 
+        System.out.println(ch+0); // Type Conversion
+        int a = ch; //Implicit Type Casting
+        System.out.println(a); // Type Conversion
+        // Integer to Character
+        int x=4;
+        char ch1=(char) x; //Explicit Type Casting
+        System.out.println(ch1);
+    }
+
+}
