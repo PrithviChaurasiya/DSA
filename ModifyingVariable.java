22ecedefca32571113b1b053b4f338fddba3bd98
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-03-26 11:32:22 +0530

    Add Modifying Variables

---
+++ ModifyingVariable.java
@@ -0,0 +1,19 @@
+package DSA;
+
+public class ModifyingVariable {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println(x);
+        x = x + 10;
+        System.out.println(x);
+        x = x - 10;
+        System.out.println(x);
+        x = x * 10;
+        System.out.println(x);
+        x = x / 10;
+        System.out.println(x);
+        x = x % 10;
+        System.out.println(x);
+
+    }
+}
