6091c0178b8b053dd52b332f662040db6454e363
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-03-26 23:12:19 +0530

    Add Ternary file

---
+++ IfElse/TernaryOperator.java
@@ -0,0 +1,12 @@
+package DSA.IfElse;
+
+import java.util.Scanner;
+
+public class TernaryOperator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int n = sc.nextInt();
+        System.out.println((n%2==0) ? "Even" : "Odd");
+    }
+}
