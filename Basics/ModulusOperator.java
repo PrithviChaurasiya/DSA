e746e2df9a6c47532535a6ffc46a29f4207ccc1e
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-03-26 19:51:34 +0530

    Add Modulus Operator file

---
+++ Basics/ModulusOperator.java
@@ -0,0 +1,11 @@
+package DSA.Basics;
+
+public class ModulusOperator {
+    public static void main(String[] args) {
+        System.out.println(10%8);
+        System.out.println(8%10);
+        System.out.println(-10%8); //-(10%8)
+        System.out.println(10%-8); //10%8
+        System.out.println(-39%-10);//-(39%10)
+    }
+}
