d4c3d518564d42007513cbbbeaa8ca683dc78351
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-03-26 12:10:25 +0530

    Add Input File

---
+++ Basics/InputFromUser.java
@@ -0,0 +1,10 @@
+package DSA.Basics;
+import java.util.Scanner;
+public class InputFromUser {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter A Number : ");
+        int S = sc.nextInt();
+        System.out.println(S);
+    }
+}
