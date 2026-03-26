a52f3fa81200108ba03fa75d263e9b1fc1d4d6d8
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-03-26 21:29:44 +0530

    Add Relational Operator file

---
+++ Basics/RelationalOperater.java
@@ -0,0 +1,14 @@
+package DSA.Basics;
+
+public class RelationalOperater {
+    public static void main(String[] args) {
+        System.out.println(5>9);
+        System.out.println(5<9);
+        System.out.println(5<=5);
+        System.out.println(5<5);
+        System.out.println(5>5);
+        System.out.println(5==5);
+        System.out.println(5==9);
+        System.out.println(5!=7);
+    }
+}
