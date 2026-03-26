ec6e39744fe79cccc55d17f29ceab0446841fde8
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-03-26 11:09:45 +0530

    Add variable

---
+++ Variables.java
@@ -0,0 +1,12 @@
+package DSA;
+
+public class Variables {
+    public static void main(String[] args) {
+        int x = 5; //Initialization and Decleration in the Same Line
+        System.out.print(x);
+        int y; //Initialization
+        y = 23; //Decleraion
+        System.out.println(y);
+
+    }
+}
