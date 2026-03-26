c851e35c6dedc9684adc8ad080fcc8550f13e656
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-03-26 22:02:38 +0530

    Add Intger file

---
+++ CheckInteger.java
@@ -0,0 +1,16 @@
+package DSA;
+
+import java.util.Scanner;
+
+public class CheckInteger {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Element To Check It Is Integer : ");
+        double n = sc.nextDouble();
+        if(n==(int)n)
+            System.out.println("The Given Number Is Integer");
+        else
+            System.out.println("The Given Number Is Not An Integer");
+
+    }
+}
