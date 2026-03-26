0b531a490bca979f7731bd917a320d5d320cdca9
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-03-26 20:31:07 +0530

    Add Increment And Decrement Operator file

---
+++ Basics/IncrementAndDecrement.java
@@ -0,0 +1,15 @@
+package DSA.Basics;
+
+public class IncrementAndDecrement {
+    public static void main(String[] args) {
+        int x = 53;
+        System.out.println(x);
+        System.out.println(++x);
+        System.out.println(x++);
+
+        int y = 67;
+        System.out.println(y);
+        System.out.println(y--);
+        System.out.println(--y);
+    }
+}
