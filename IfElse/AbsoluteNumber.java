258cb58e8aa8530d2cc5f4049ff02b5d289d61e5
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-03-26 21:51:16 +0530

    Add Absolute file

---
+++ IfElse/AbsoluteNumber.java
@@ -0,0 +1,15 @@
+package DSA.IfElse;
+
+import java.util.Scanner;
+
+public class AbsoluteNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Element To Print Absolute Value : ");
+        int n = sc.nextInt();
+        if(n>0)
+            System.out.println(n);
+        else
+            System.out.println(n*(-1));
+    }
+}
