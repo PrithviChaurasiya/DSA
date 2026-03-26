4669407c92152574f516c9cedbb145a610fce890
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-03-26 11:54:19 +0530

    Add Dvision File

---
+++ Basics/Division_Operator.java
@@ -0,0 +1,15 @@
+package DSA.Basics;
+
+public class Division_Operator {
+    public static void main(String[] args) {
+        double x = 395;
+        double y = 10;
+        System.out.println(x/y);
+        System.out.println(5/y);
+        System.out.println(x/10);
+        System.out.println(98/10);
+        System.out.println(98/10.0);
+        System.out.println(9.8/10);
+        
+    }
+}
