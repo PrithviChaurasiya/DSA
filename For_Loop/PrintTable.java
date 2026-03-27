0a5a31ff121bed57e99652549b5cd7ecfe90eede
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-03-27 17:10:17 +0530

    Add Table Loop File

---
+++ For_Loop/PrintTable.java
@@ -0,0 +1,15 @@
+package DSA.For_Loop;
+
+import java.util.Scanner;
+
+public class PrintTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n*i);
+        }
+        sc.close();
+    }
+}
