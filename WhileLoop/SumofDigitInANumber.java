d17187cc56ccf20ece3622c3c8c7ec06f9309240
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-03-27 18:35:19 +0530

    Add Counting Digit File

---
+++ WhileLoop/SumofDigitInANumber.java
@@ -0,0 +1,16 @@
+package DSA.WhileLoop;
+import java.util.Scanner;
+public class SumofDigitInANumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int n = sc.nextInt();
+        int sum=0;
+        while(n!=0){
+            sum=sum+n%10;
+            n=n/10;
+        }
+        System.out.println(sum);
+        sc.close();
+    }
+}
