87d9df802a8250da895000c9e2ad9938a45db193
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-03-27 17:21:31 +0530

    Add Table Loop File

---
+++ For_Loop/APSeries.java
@@ -0,0 +1,20 @@
+package DSA.For_Loop;
+
+import java.util.Scanner;
+
+public class APSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First Number of AP : ");
+        int a = sc.nextInt();
+        System.out.print("Enter Difference between Number : ");
+        int d = sc.nextInt();
+        System.out.print("Enter Number of Terms : ");
+        int n = sc.nextInt();
+
+        for (int i = a; i <= (a+(n-1)*d); i+=d) {
+            System.out.println(i+" ");
+        }
+        sc.close();
+    }
+}
