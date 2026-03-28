3efad156139324bd58b167e75f1c910c99ea3760
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-03-28 22:34:32 +0530

    Add Sum Fn file

---
+++ Methods/SumFn.java
@@ -0,0 +1,10 @@
+package DSA.Methods;
+
+public class SumFn {
+    public static void sum(int a,int b,int c){
+        System.out.println(a+b+c); 
+    }
+    public static void main(String[] args) {
+        sum(4,3,6);
+    }
+}
