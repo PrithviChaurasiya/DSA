556d9776780a363f1f1723c3a991ecb53af4e8b1
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-03-28 13:58:50 +0530

    Add Vertical Flipped Triangle file

--- PatternPrinting/VerticalFlipedTriangle.java
+++ PatternPrinting/VerticalFlipedTriangle.java
@@ -16,6 +16,17 @@ public class VerticalFlipedTriangle {
             }
             System.out.println();
         }
+        
+        // Another Method
+        // for (int i = 1; i <= r; i++) {
+        //     for (int j = 1; j <= r ; j++) {
+        //         if((i+j)>r)
+        //             System.out.print("* ");
+        //         else
+        //             System.out.print("  ");
+        //     }
+        //     System.out.println();
+        // }
         sc.close();
     }
 }
