494ad71b032467ae4fc7dbae45aa0c62225a0e0e
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-03-28 11:33:21 +0530

    Add Square Star File

---
+++ PatternPrinting/StarSqare.java
@@ -0,0 +1,18 @@
+package DSA.PatternPrinting;
+
+import java.util.Scanner;
+
+public class StarSqare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int r = sc.nextInt();
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < r; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
