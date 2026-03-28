b1794d88081e7025df43b2dfc04f933607e5fc99
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-03-28 11:54:13 +0530

    Add Star Triangle file

---
+++ PatternPrinting/StarTriangle.java
@@ -0,0 +1,18 @@
+package DSA.PatternPrinting;
+
+import java.util.Scanner;
+
+public class StarTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Height : ");
+        int r = sc.nextInt();
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
