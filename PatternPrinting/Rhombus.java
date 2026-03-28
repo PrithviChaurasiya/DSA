d5635bbbf9bc6e76a190e1d926899601cd55930e
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-03-28 14:12:21 +0530

    Add Rhombus file

---
+++ PatternPrinting/Rhombus.java
@@ -0,0 +1,21 @@
+package DSA.PatternPrinting;
+
+import java.util.Scanner;
+
+public class Rhombus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Height : ");
+        int r = sc.nextInt();
+        for (int i = 1; i <= r; i++) {
+            for (int j = r; j > i; j--) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= r; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
