5965ab32526bc16df469ad91943f68c7f6159876
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-03-28 17:29:33 +0530

    Add Bridge Pattern File

---
+++ PatternPrinting/Bridge.java
@@ -0,0 +1,28 @@
+package DSA.PatternPrinting;
+
+import java.util.Scanner;
+
+public class Bridge {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Height : ");
+        int r = sc.nextInt();
+        for (int j = 1; j < (2*r); j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        for (int i = 1; i < r; i++) {
+            for (int j = r; j > i; j--) {
+                System.out.print("* ");
+            }
+            for (int j = 1; j <= (2*i-1); j++) {
+                System.out.print("  ");
+            }
+            for (int j = r; j > i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
