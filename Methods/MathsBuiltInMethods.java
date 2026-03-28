220d136243420c50c9723ea971191f855ca71070
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-03-28 21:33:20 +0530

    Add Maths file

---
+++ Methods/MathsBuiltInMethods.java
@@ -0,0 +1,15 @@
+package DSA.Methods;
+
+public class MathsBuiltInMethods {
+    public static void main(String[] args) {
+        System.out.println(Math.sqrt(4));
+        System.out.println(Math.cbrt(100));
+        System.out.println(Math.PI);
+        System.out.println(Math.abs(-234.66));
+        System.out.println(Math.ceil(3.4));
+        System.out.println(Math.floor(99.986));
+        System.out.println(Math.pow(3, 2));
+        System.out.println(Math.min(3, 9));
+        System.out.println(Math.max(3, 9));
+    }
+}
