f7ce80c5c99738efbb185ff9a21e3b0bbb1ca869
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-03-28 22:30:50 +0530

    Add USer defined file

---
+++ Methods/UserDefinedFn.java
@@ -0,0 +1,12 @@
+package DSA.Methods;
+
+public class UserDefinedFn {
+    //Function Decleration
+    public static void name(){
+        System.out.println("Hello You Successfully Created User Defined Function ");
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        name(); //Function Call
+    }
+}
