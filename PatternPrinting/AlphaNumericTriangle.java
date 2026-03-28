46fa9c4800c49c6ab52f6873552e05243cb97c85
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-03-28 12:01:42 +0530

    Add Alphanumeric Triangle file

---
+++ PatternPrinting/AlphaNumericTriangle.java
@@ -0,0 +1,22 @@
+package DSA.PatternPrinting;
+
+import java.util.Scanner;
+
+public class AlphaNumericTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Row : ");
+        int r = sc.nextInt();
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (i%2==0) {
+                    System.out.print((char)(64+j)+" ");
+                }
+                else
+                    System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
