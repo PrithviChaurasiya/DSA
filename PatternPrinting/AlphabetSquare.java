8fdd424e8f19050421200307bcc54bfc7db27330
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-03-28 11:45:15 +0530

    Add Square Alphabet File

---
+++ PatternPrinting/AlphabetSquare.java
@@ -0,0 +1,18 @@
+package DSA.PatternPrinting;
+
+import java.util.Scanner;
+
+public class AlphabetSquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Row : ");
+        int r = sc.nextInt();
+        for (int i = 0; i < r; i++) {
+            for (int j = 1; j <= r; j++) {
+                System.out.print((char)(64+j)+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
