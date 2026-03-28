0bfafbd8ff8a249ff4028284629c71cb848f1c29
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-03-28 14:23:33 +0530

    Add Pyramid file

---
+++ PatternPrinting/Pyramid.java
@@ -0,0 +1,21 @@
+package DSA.PatternPrinting;
+
+import java.util.Scanner;
+
+public class Pyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Height : ");
+        int r = sc.nextInt();
+        for (int i = 1; i <= r; i++) {
+            for (int j = r; j > i; j--) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= (2*i-1); j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
