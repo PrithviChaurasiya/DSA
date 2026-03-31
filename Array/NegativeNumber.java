7b3a24cdf8993feabcbb6d6bded20dfd7557c304
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-03-31 15:58:57 +0530

    Add Negative Numbers in an Array File

--- Array/NegativeNumber.java
+++ Array/NegativeNumber.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class NegativeNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter The Sze of an Array");
+        System.out.print("Enter The Sze of an Array : ");
         int n = sc.nextInt();
         int[] arr = new int[n];
-        System.out.println("Enter the elements in an array ");
+        System.out.print("Enter The Elements In An Array : ");
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
