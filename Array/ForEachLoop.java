1a831e279ca707bdb98d88dd36f7af82582ea536
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-04-08 16:50:25 +0530

    Add For each loop file

---
+++ Array/ForEachLoop.java
@@ -0,0 +1,12 @@
+package DSA.Array;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
+ //for each loop is used to traverse the data structures
+ //we cannot update the values of an array using for each loop
