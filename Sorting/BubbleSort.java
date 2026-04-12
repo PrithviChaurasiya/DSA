ff20f0cc7a82f82a7851196fa42ffaa8edca1d78
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-04-12 15:39:52 +0530

    Add Bubble Sort File

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,29 @@
+package DSA.Sorting;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the Number of Elements : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n-1];
+        System.out.println("Enter the elements in an array : ");
+        for (int i = 0; i < n-1; i++) {
+            arr[i]=sc.nextInt();
+        }
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length-1-i; j++) {
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+                    System.out.print(arr[i]+" ");
+        }
+        sc.close();
+    }
+}
