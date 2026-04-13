288d28a26b6054b9931480ad0408cd1edf4733fa
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-04-13 18:39:39 +0530

    Add Selection Sort

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,31 @@
+package DSA.Sorting;
+
+import java.util.Scanner;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the Number of Elements : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements in an array : ");
+        for (int i = 0; i < n; i++) {
+            arr[i]=sc.nextInt();
+        }
+        for (int i = 0; i < n-1; i++) {
+            int min=i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j]<arr[min]) {
+                    min=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        sc.close();
+    }
+}
