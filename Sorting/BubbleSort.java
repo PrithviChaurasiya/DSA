099cf294da62dada9e0fce3e63d6b17b0a737966
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-04-13 11:12:56 +0530

    Add modified bubble sort

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -13,12 +13,17 @@ public class BubbleSort {
             arr[i]=sc.nextInt();
         }
         for (int i = 0; i < arr.length-1; i++) {
+            int swap=0;
             for (int j = 0; j < arr.length-1-i; j++) {
                 if(arr[j]>arr[j+1]){
                     int temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
+                    swap++;
                 }
+            }
+            if (swap==0) {
+                break;
             }
         }
         for (int i = 0; i < arr.length; i++) {
