b78a6a84a85f4d582c96fa69190a275c477d7406
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-04-13 19:54:59 +0530

    Add Common Element In Two Sorted Array

---
+++ Sorting/CommonElement.java
@@ -0,0 +1,42 @@
+package DSA.Sorting;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CommonElement {
+    public static void main(String[] args) {
+        int m,n;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of th 1st array : ");
+        m=sc.nextInt();
+        System.out.print("Enter the size of th 2nd array : ");
+        n=sc.nextInt();
+        int[] a = new int[m];
+        int[] b = new int[n];
+        ArrayList<Integer> ans=new ArrayList<>();
+        System.out.println("Enter the elements in 1st array : ");
+        for (int i = 0; i < m; i++) 
+            a[i]=sc.nextInt();
+        System.out.println("Enter the elements in 2nd array : ");
+        for (int i = 0; i < n; i++) 
+            b[i]=sc.nextInt();
+        int i=0,j=0;
+        Arrays.sort(a);
+        Arrays.sort(b);
+        while (i<m && j<n) {
+            if(a[i]==b[j]){
+                ans.add(a[i]);
+                i++;
+                j++;
+            }
+            else if(a[i]<b[j]){
+                i++;
+            }
+            else
+                j++;
+        }
+        System.out.println(ans);
+        sc.close();
+    }
+}
