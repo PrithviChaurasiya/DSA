3dad017968f816846fb211fcf6f7b6c1886a0242
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-04-13 11:22:23 +0530

    Add modified bubble sort

--- Sorting/ReverseBubbleSort.java
+++ Sorting/ReverseBubbleSort.java
@@ -2,7 +2,7 @@ package DSA.Sorting;
 
 import java.util.Scanner;
 
-public class BubbleSort {
+public class ReverseBubbleSort {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         System.out.print("Enter the Number of Elements : ");
@@ -15,7 +15,7 @@ public class BubbleSort {
         for (int i = 0; i < n-1; i++) {
             int swap=0;
             for (int j = 0; j < n-1-i; j++) {
-                if(arr[j]>arr[j+1]){
+                if(arr[j]<arr[j+1]){
                     int temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
@@ -26,7 +26,7 @@ public class BubbleSort {
                 break;
             }
         }
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < arr.length; i++) {
                     System.out.print(arr[i]+" ");
         }
         sc.close();
