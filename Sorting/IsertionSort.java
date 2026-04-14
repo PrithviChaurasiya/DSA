b75e3a0e885b5b7d256cf8ce07c9ded4f1d0e427
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-04-14 11:38:19 +0530

    Add Insertion Sort

--- Sorting/IsertionSort.java
+++ Sorting/IsertionSort.java
@@ -12,7 +12,7 @@ public class IsertionSort {
         for (int i = 0; i < n; i++) {
             arr[i]=sc.nextInt();
         }
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 1; i < arr.length; i++) {
             for (int j = i; j > 0; j--) {
                 if (arr[j]<arr[j-1]) {
                     int temp = arr[j];
@@ -24,6 +24,18 @@ public class IsertionSort {
         for (int i = 0; i < n; i++) {
                     System.out.print(arr[i]+" ");
         }
+
+        // Another Code
+        for (int i = 1; i < arr.length; i++) {
+            int j=i;
+            while (j>0 && arr[j]<arr[j-1]) {
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+                j--;
+            }
+        }
+
         sc.close();
     }
 }
