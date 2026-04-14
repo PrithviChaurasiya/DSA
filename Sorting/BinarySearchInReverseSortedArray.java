866e161c1516a2d7fa2e6f6c95ea931d34708c70
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-04-14 18:27:26 +0530

    Add Binary Search in reversed sorted array

---
+++ Sorting/BinarySearchInReverseSortedArray.java
@@ -0,0 +1,38 @@
+package DSA.Sorting;
+
+import java.util.Scanner;
+
+public class BinarySearchInReverseSortedArray {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the Number of Elements : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements in an array : ");
+        for (int i = 0; i < n; i++) {
+            arr[i]=sc.nextInt();
+        }
+        System.out.print("Enter the target element : ");
+        int target=sc.nextInt();
+        int left=0,right=arr.length;
+        boolean flag=false;
+        while (left<=right) {
+            int mid=(left+right)/2;
+            if(arr[mid]==target){
+                flag=true;
+                break;
+            }
+
+            else if(arr[mid]<target)
+                right=mid-1;
+            else
+                left=mid+1;
+        }
+        if(flag) System.out.println("the target is found ");
+        else System.out.println("the target is not found ");
+        for (int i = 0; i < n; i++) {
+                    System.out.print(arr[i]+" ");
+        }
+        sc.close();
+    }
+}
