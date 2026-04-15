57ee841f1eb2697233ab78c979bb6516d362288a
PrithviChaurasiya <prithvichaurasiya.01@gmail.com>
2026-04-15 17:51:12 +0530

    Add Floor element in an array

---
+++ SortingAndSearching/FloorinArray.java
@@ -0,0 +1,30 @@
+package DSA.SortingAndSearching;
+
+import java.util.Scanner;
+
+public class FloorinArray {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the Number of Elements : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements in an array : ");
+        for (int i = 0; i < n; i++) {
+            arr[i]=sc.nextInt();
+        }
+        System.out.print("Enter the element : ");
+        int ele=sc.nextInt();
+        int left=0,right=arr.length-1,idx=-1;
+        while (left<=right) {
+            int mid=(left+right)/2;
+            if(arr[mid]>ele)
+                right=mid-1;
+            else if(arr[mid]<=ele){
+                idx=mid;
+                left=mid+1;
+            }
+        }
+        System.out.println(idx);
+        sc.close();
+    }
+}
